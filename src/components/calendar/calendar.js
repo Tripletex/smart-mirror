c376ee1d321c6efad64a93d1700fa02ed5631bf2
Harry Nystad <harryjnystad@gmail.com>
2018-11-08 15:53:25 +0100

    some minor fixes or not :S

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -9,24 +9,26 @@ class Calendar extends Component {
 		<div className="calendar">
 			Google Kalender intergasjon
 			<table>
-				<tr>
-					<td>&#9873;</td>
-					<td>21.08</td>
-					<td>09:00</td>
-					<td>Hackaton demo</td>
-				</tr>
-				<tr>
-					<td>&#9873;</td>
-					<td>21.08</td>
-					<td>11:00</td>
-					<td>lunch</td>
-				</tr>
-				<tr>
-					<td>&#9873;</td>
-					<td>21.08</td>
-					<td>11:30</td>
-					<td>Hackaton demo del 2</td>
-				</tr>
+				<tbody>
+					<tr>
+						<td>&#9873;</td>
+						<td>21.08</td>
+						<td>09:00</td>
+						<td>Hackaton demo</td>
+					</tr>
+					<tr>
+						<td>&#9873;</td>
+						<td>21.08</td>
+						<td>11:00</td>
+						<td>lunch</td>
+					</tr>
+					<tr>
+						<td>&#9873;</td>
+						<td>21.08</td>
+						<td>11:30</td>
+						<td>Hackaton demo del 2</td>
+					</tr>
+				</tbody>
 			</table>
 		</div>
     );
