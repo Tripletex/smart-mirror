e844996ea6a9718f332ead0048a870d7d342f4d2
Harry Nystad <harryjnystad@gmail.com>
2018-11-13 13:22:41 +0100

    styling for iframe

--- src/Components/EnTur/EnTur.js
+++ src/Components/EnTur/EnTur.js
@@ -10,7 +10,7 @@ class EnTur extends Component {
 
 	render() {
 		return (
-			<iframe className="enTur" src="https://mon.ruter.no/monitor/3012500/Sk%C3%B8yen%20tog/?clock=false" frameBorder="0" width="800" height="600"/>
+			<iframe className="enTur" src="https://mon.ruter.no/monitor/3012500/Sk%C3%B8yen%20tog/?clock=false&rows=4" frameBorder="0" width="100%" height="600"/>
 		)
 	}
 }
