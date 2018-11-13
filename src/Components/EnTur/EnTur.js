3b2f80e23b1562882fe9ec940f3ccdde0bb95493
Harry Nystad <harryjnystad@gmail.com>
2018-11-13 12:38:45 +0100

    ruter iframe

---
+++ src/Components/EnTur/EnTur.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+import './EnTur.css'
+
+
+
+class EnTur extends Component {
+	
+
+	render() {
+		return (
+			<iframe className="enTur" src="https://mon.ruter.no/monitor/3012500/Sk%C3%B8yen%20tog/3012501/Skøyen%20buss?clock=false" frameBorder="0" width="1400" height="600"/>
+		)
+	}
+}
+export default EnTur;
