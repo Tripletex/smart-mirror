7dcdee9ade194e36f17c55a7fcfd52cb00d8926b
hn <harry@tripletex.no>
2020-09-02 17:06:30 +0200

    restart the project

--- src/index.js
+++ src/index.js
@@ -3,19 +3,11 @@ import ReactDOM from "react-dom";
 
 import './Common/style.css'
 import Clock from './Components/Clock/clock'
-import EnTur from './Components/EnTur/EnTur'
-import Var from './Components/Var/Var'
-import Weather from "./Components/Weather/weather";
-import Statuspage from './Components/Statuspage/Statuspage'
 
 const Index = () => {
   return (
 	  <div>
-		<EnTur />
-		<Clock />
-		 <Weather/>
-		  <Statuspage/>
-		  <Var />
+		  <Clock />
 	  </div>
   )
 };
